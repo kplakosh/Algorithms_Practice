76f060b39685c04788b5ab38248125b41b47f01b
kplakosh <kplakosh@gmail.com>
2019-04-05 14:05:50 -0700

    implemented function for reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+
+// function reverseInt(n) {
+//     if (n < 0) {
+//         n *= -1;
+//         return parseInt(n.toString().split('').reverse().join('')) * -1;
+//     } else {
+//         return parseInt(n.toString().split('').reverse().join(''));
+//     }
+// }
