0d903e05356c412bd7ecd37ceef313f47bd8f9d4
kplakosh <kplakosh@gmail.com>
2019-04-29 14:05:15 -0700

    implementation of Steps function

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,14 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) { 
-    for (let i = 0; i < n; i++) {
-        let str = '';
-        for (let j = n; j > 0; j--) {
-            str += '#'.repeat(i) + ' '.repeat(j - 1);
-            console.log(str);
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
         }
+
+        console.log(stair);
     }
 }
+
+module.exports = steps;
+
+
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+
+//   // const add = stair.length <= row ? '#' : ' ';
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//   steps(n, row, stair);
+// }
 
 module.exports = steps;
